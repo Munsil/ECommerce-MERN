f16ffb1726b6e6490cd14ff8d744a23d5a8e4368
Munsil <munzirramo18@gmail.com>
2023-12-05 12:22:05 +0530

    Initial Commit

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,6 @@
 const User= require("../models/userModel");
 const asyncHandler = require("express-async-handler");
-const createUser = async (req, res) =>{
+const createUser = asyncHandler(async (req, res) =>{
     const email = req.body.email;
     const findUser = await User.findOne({email : email});
     if(!findUser){
@@ -8,11 +8,8 @@ const createUser = async (req, res) =>{
         res.json(newUser);
     }
     else{
-        res.json({
-            msg: "User Already Exists",
-            success : false,
-        });
+        throw new Error("User Alrady Exists");
     }
-};
+});
 
 module.exports = {createUser};
