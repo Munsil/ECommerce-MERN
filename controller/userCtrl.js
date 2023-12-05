e5e712962c1cbc20b6fdd5955d7020a6a7324f23
Munsil <munzirramo18@gmail.com>
2023-12-05 16:16:30 +0530

    Token Added

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken");
 const User= require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const createUser = asyncHandler(async (req, res) =>{
@@ -16,7 +17,15 @@ const loginUserCtrl=asyncHandler(async (req, res) => {
     const {email,password}=req.body;
     const findUser=await User.findOne({email});
     if(findUser && await findUser.isPasswordMatched(password)){
-        res.json(findUser);
+        res.json({
+            id: findUser?._id,
+            firstname: findUser?.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            token:generateToken(findUser?._id),
+
+        });
     }else{
         throw new Error("Invalid Credentials");
     }
