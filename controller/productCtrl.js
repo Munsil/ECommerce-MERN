c168185d0f8419a23e2af780f6deb63058ef2247
Munsil <munzirramo18@gmail.com>
2024-01-17 08:41:20 +0530

    Delete Product Added

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -34,6 +34,18 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 });
 
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deleteProduct = await Product.findOneAndDelete(id);
+        res.json(deleteProduct);
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+
+});
+
 const getaProduct = asyncHandler(async (req, res) => {
     const { id } = req.params;
     try {
@@ -56,4 +68,4 @@ const getAllProduct = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createProduct, getaProduct, getAllProduct, updateProduct };
+module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct };
